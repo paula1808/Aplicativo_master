c4b6237d7e8cd10a0dcc630649a3e4b1a040aa18
PaulaGalindoCh <paula.galindo@usco.edu.co>
2025-07-11 22:25:33 -0400

    proyecto despliegue

---
+++ src/test/java/com/play_learn/learn_topic/LearnTopicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.play_learn.learn_topic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnTopicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
